fd31fa55b5727bf75798766afe19a4d85f59b9c0
Samir Aes <samirgonzaloaeslopez@outlook.com>
2025-03-15 12:22:04 -0600

    arreglando enviado de email

--- SendEmail.php
+++ SendEmail.php
@@ -1,14 +1,24 @@
 
 <?php
-$para = $_POST["email"]; // Tomar el email del formulario
-$asunto = "Resultado Dosha";
-$mensaje = "Hola, este es el resultado de tu dosha: .";
-$cabeceras = "From:  doshas@centroayurveda.mx" . "\r\n" . phpversion();
-if (mail($para, $asunto, $mensaje, $cabeceras)) {
-    header("Location: ResultadoDoshas.html"); 
-    exit();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $para = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); // Sanitizar el email
+    $asunto = "Resultado Dosha";
+    $mensaje = "Hola, este es el resultado de tu dosha.";
+    $cabeceras = "From: doshas@centroayurveda.mx" . "\r\n" . "Reply-To: doshas@centroayurveda.mx" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+
+    echo "<script>console.log('Email recibido: " . $_POST["email"] . "');</script>";
+
+
+    if (filter_var($para, FILTER_VALIDATE_EMAIL)) {
+        if (mail($para, $asunto, $mensaje, $cabeceras)) {
+            echo "Correo enviado correctamente.";
+        } else {
+            echo "Error al enviar el correo.";
+        }
+    } else {
+        echo "Dirección de correo no válida.";
+    }
 } else {
-    header("Location: Ingreso.html"); 
-    exit();
+    echo "Método no permitido.";
 }
 ?>
